69bbbc9aa6fb89c113bd953c3db7cec372a3a36e
Nikolai <klikclic@mail.ru>
2021-08-07 16:44:56 +0300

    First test commit

---
+++ js/script.js
@@ -0,0 +1,40 @@
+"use strict";
+
+/* 
+
+let number = 5;
+
+console.log(number);
+
+const obj = {
+    a: 50
+};
+
+obj.a = 10;
+
+console.log(obj);
+
+console.log(name);
+
+var name = "Ivan";
+
+{
+    var result = 50;
+}
+
+console.log(result);
+
+const human = {
+    name: "John",
+    age: 25,
+    isMarried: false
+};
+
+console.log(human.name);
+console.log(human.age);
+
+let arr = ['plum.png', 'orange.jpg', 6, 'apple.bmp', {}, []]; //Массив может содержать в себе любые типы данных в том числе и объекты и другие массивы
+console.log(arr[4]);
+
+*/
+
