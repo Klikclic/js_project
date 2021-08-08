f5245b29118b0f1d31da5368a2cd5c4676b6eda0
Nikolai <klikclic@mail.ru>
2021-08-08 12:38:03 +0300

    add circle for and check count films

--- js/script.js
+++ js/script.js
@@ -1,21 +1,78 @@
 "use strict";
 
-const numberOfFlims = +prompt('Сколько фильмов вы уже посмотрели?', '');
+const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
 
 const personalMovieDB = {
-    count: numberOfFlims,
+    count: numberOfFilms,
     movies: {},
     actors: {},
     genres: [],
     privat: false
 };
 
-const lastViewMovie0 = prompt('Один из последних просмотренных фильмов?', ''),
+// Реализация ввода без цикла, требует 4 переменных вместо двух
+/* const lastViewMovie0 = prompt('Один из последних просмотренных фильмов?', ''),
       ratingMovie0 = prompt('На сколько оцените его?', ''),
       lastViewMovie1 = prompt('Один из последних просмотренных фильмов?', ''),
       ratingMovie1 = prompt('На сколько оцените его?', '');
 
 personalMovieDB.movies[lastViewMovie0] = ratingMovie0;
-personalMovieDB.movies[lastViewMovie1] = ratingMovie1;
+personalMovieDB.movies[lastViewMovie1] = ratingMovie1; */
+
+for (let i = 0; i <= 1; i++) {
+    const lastViewMovie = prompt('Один из последних просмотренных фильмов?', ''),
+          ratingMovie = prompt('На сколько оцените его?', '');
+
+    if (lastViewMovie != null && ratingMovie != null && lastViewMovie != '' && ratingMovie != '' && lastViewMovie.length < 50) {
+        personalMovieDB.movies[lastViewMovie] = ratingMovie;
+        console.log('Done!');
+    } else {
+        console.log('Error');
+        i--;
+    }
+}
+
+//Альтернативная реализация цикла с ипользование while
+/* let i = 0;
+while (i <= 1) {
+    const lastViewMovie = prompt('Один из последних просмотренных фильмов?', ''),
+    ratingMovie = prompt('На сколько оцените его?', '');
+
+    if (lastViewMovie != null && ratingMovie != null && lastViewMovie != '' && ratingMovie != '' && lastViewMovie.length < 50) {
+    personalMovieDB.movies[lastViewMovie] = ratingMovie;
+    console.log('Done!');
+    } else {
+    console.log('Error');
+    i--;
+    }
+    i++;
+} */
+
+//Альтернативная реализация цикла с ипользование do while
+/* let i = 0;
+do {
+    const lastViewMovie = prompt('Один из последних просмотренных фильмов?', ''),
+    ratingMovie = prompt('На сколько оцените его?', '');
+
+    if (lastViewMovie != null && ratingMovie != null && lastViewMovie != '' && ratingMovie != '' && lastViewMovie.length < 50) {
+    personalMovieDB.movies[lastViewMovie] = ratingMovie;
+    console.log('Done!');
+    } else {
+    console.log('Error');
+    i--;
+    }
+    i++; 
+}
+while (i <= 1); */
+
+if (personalMovieDB.count < 10) {
+    console.log('Просмотрено довольно мало фильмов');
+} else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
+    console.log('Вы классический зритель');
+} else if (personalMovieDB.count >= 30) {
+    console.log('Вы киноман');
+} else {
+    console.log('Произошла ошибка');
+}
 
 console.log(personalMovieDB);
